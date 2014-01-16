3e9272ba247e6ba22d8f45041e5ae81f9d39f386
Denis Garcia <denisgarci@gmail.com>
2014-01-16 05:39:42 -0500

    Exercise 8

--- ex7.c
+++ ex7.c
@@ -21,5 +21,22 @@ int main(int argc, char *argv[])
     int care_percentage = bugs * nul_byte;
     printf("Which means you should care %d%%.\n", care_percentage);
 
+    // Break It
+    //printf("Test with %%s ,%s.\n", nul_byte);
+    printf("Test with %%c, %c.\n", nul_byte);
+
+    // Extra Credit
+    long test = 1;
+    printf("Test %ld.\n", test);
+
+    long a_big_long = 100000000000000000000000;
+    printf("A big long %ld.\n", a_big_long);
+
+    long two_to_32 = 4294967296;
+    printf("2 to the 32 %ld.\n", two_to_32);
+
+    unsigned long u_long = 100000000000000000000000;
+    printf("A unsigned long %ld.\n", u_long);
+
     return 0;
 }
